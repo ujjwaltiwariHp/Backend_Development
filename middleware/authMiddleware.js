1d2e4ce48e08254a299911dd293ed458ca442c67
ujjwaltiwariHp <ujjwaltiwari.hp@gmail.com>
2025-08-01 11:07:27 +0530

    user_Id bug fixed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -21,7 +21,7 @@ const authMiddleware = async (req, res, next) => {
       return res.status(403).json({ error: 'Token expired or invalid' });
     }
 
-    req.user = decoded; // now you can use req.user in protected routes
+    req.userId = decoded.id;
     next();
   } catch (err) {
     return res.status(403).json({ error: 'Invalid or expired token' });
