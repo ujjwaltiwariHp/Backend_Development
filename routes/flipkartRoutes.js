f857830b82b2337c2d3522108695cd2f4a883ce2
ujjwaltiwariHp <ujjwaltiwari.hp@gmail.com>
2025-08-06 12:16:42 +0530

    Scraped flipcart full product details(dummy)

--- routes/flipkartRoutes.js
+++ routes/flipkartRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { fetchFlipkartMobiles } = require('../controllers/flipkartController');
+const { fetchFlipkartMobileFull } = require('../controllers/flipkartFullScraper');
 
 router.get('/fetch/flipkart/mobile', fetchFlipkartMobiles);
+router.get('/fetch/flipkart/mobile/full', fetchFlipkartMobileFull);
 
 module.exports = router;
 
